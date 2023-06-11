cea1114e64d7d881665bffd794ad1535c4c2db69
Oussama IOUIRY <oussamaiouiry@gmail.com>
2023-06-11 16:44:11 +0100

    Add setOnTouchListener to User item to implement DoubleClick

--- app/src/main/java/com/example/mini_projet_01/UsersAdapter.java
+++ app/src/main/java/com/example/mini_projet_01/UsersAdapter.java
@@ -2,22 +2,19 @@ package com.example.mini_projet_01;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class UsersAdapter extends BaseAdapter {
+    final int DOUBLE_CLICK_TIMEOUT = 250;
     ArrayList<User> users;
 
     LayoutInflater inflater;
@@ -51,9 +48,11 @@ public class UsersAdapter extends BaseAdapter {
 
         TextView tv_itemUsersFullName = view.findViewById(R.id.tv_itemUsersFullName);
         TextView tv_itemUsersCity = view.findViewById(R.id.tv_itemUsersCity);
+        ImageView iv_itemUsersCheck = view.findViewById(R.id.iv_itemUsersCheck);
 
         tv_itemUsersFullName.setText(users.get(i).fullName());
         tv_itemUsersCity.setText(users.get(i).getCity());
+
         view.setOnLongClickListener(v -> {
             AlertDialog builder = new AlertDialog.Builder(context)
                     .setTitle(String.format("User %d", i + 1))
@@ -61,6 +60,24 @@ public class UsersAdapter extends BaseAdapter {
                     .show();
 
             return false;
+        });
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            long lastClickTime = 0;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    long clickTime = System.currentTimeMillis();
+                    if ((clickTime - lastClickTime) <= DOUBLE_CLICK_TIMEOUT) {
+                        iv_itemUsersCheck.setVisibility(iv_itemUsersCheck.getVisibility() == view.INVISIBLE ? View.VISIBLE : View.INVISIBLE);
+                    } else {
+                        lastClickTime = clickTime;
+                    }
+                }
+
+                return true;
+            }
         });
 
         return view;
